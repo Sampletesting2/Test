ea943c7e7c4ceff7ef7866a70dfe3534def3e073
Sampletesting2 <132168597+Sampletesting2@users.noreply.github.com>
2023-04-30 00:32:40 +0200

    Add files via upload

---
+++ ContactSubmission.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = "andrevdwalt8@gmail.com";
+    $headers = "From: ".$email;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($to,$subject,$txt,$headers);
+    header("Location: index.html?mailsent");
+}
+?>
